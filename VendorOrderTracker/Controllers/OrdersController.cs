0190f1d719e5c4acc8e3348025847a033b85b087
Anastasia Han <anastasia.han@yahoo.com>
2022-02-26 20:18:04 -0500

    Controllers added

---
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> orderList = Order.GetList();
+      return View(orderList);
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult New()
+    {
+      List<Vendor> vendorList = Order.VendorGetList();
+      return View(vendorList);
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult Create(int orderVendorId, string title, string description, int price, string orderDate, string deliveryForDate, int sourDough, int milkLoaf, int donut, int croissant, int quiche, int cheesePuff)
+    {
+      Order order = new Order(orderVendorId, title, description, price, orderDate, deliveryForDate, sourDough, milkLoaf, donut, croissant, quiche, cheesePuff);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/orders/{id}")]
+    public ActionResult Show(int id)
+      {
+        Order selectedOrder = Order.FindById(id);
+        return View(selectedOrder);
+      }
+  }
+}
---
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> vendorList = Vendor.GetList();
+      return View(vendorList);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string shipToName, string shipAddress, string shipState, string shipCity, string shipZip, string phoneNum, string email)
+    {
+      Vendor vendor = new Vendor(vendorName, shipToName, shipAddress, shipState, shipCity, shipZip, phoneNum, email);
+      return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor selectedVendor = Vendor.FindById(id);
+      List<Order> orderList = Order.GetList();
+      List<Vendor> vendorList = new List<Vendor> { selectedVendor };
+      List<Object> objectList = new List<Object> { vendorList, orderList };
+
+      return View(objectList);
+    }
+  }
+}
